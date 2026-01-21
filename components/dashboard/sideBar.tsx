0f36acf4550623d728b031c21d0d3de164fc60ec
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2026-01-21 14:40:20 +0700

    changed logo link href

--- components/dashboard/sideBar.tsx
+++ components/dashboard/sideBar.tsx
@@ -25,7 +25,7 @@ const SideBar = () => {
     <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
       <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white dark:bg-gray-900 dark:border-gray-800 px-6 pb-4">
         {/* Logo */}
-        <Link href="/" className="flex h-16 shrink-0 items-center gap-x-2 mt-2">
+        <Link href="/dashboard" className="flex h-16 shrink-0 items-center gap-x-2 mt-2">
           <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-linear-to-br from-purple-800 to-blue-600">
             <GlobeAltIcon className="h-5 w-5 text-white " />
           </div>
@@ -44,7 +44,7 @@ const SideBar = () => {
                     className={cn(
                       "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold transition-colors",
                       "text-gray-700 hover:text-primary hover:bg-gray-50",
-                      "dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-primary"
+                      "dark:text-gray-300 dark:hover:bg-gray-800 dark:hover:text-primary",
                     )}>
                     <Icon className="h-6 w-6 transition-colors" />
                     {name}
